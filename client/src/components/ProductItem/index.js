71a7560d5b081b60b8206eb374936fe0885b8c33
Alex Vazquez <avazque008@outlook.com>
2022-07-19 21:15:09 -0400

    fixed merge conflict

--- client/src/components/ProductItem/index.js
+++ client/src/components/ProductItem/index.js
@@ -1,10 +1,5 @@
 import React from "react";
 import { Link } from "react-router-dom";
-<<<<<<< HEAD
-import { pluralize } from "../../utils/helpers"
-
-function ProductItem(item) {
-=======
 import { pluralize } from "../../utils/helpers";
 import { useStoreContext } from '../../utils/GlobalState';
 import { ADD_TO_CART, UPDATE_CART_QUANTITY } from '../../utils/actions';
@@ -33,7 +28,6 @@ function ProductItem(item) {
     }
   };
 
->>>>>>> feature/global-store
   const {
     image,
     name,
@@ -55,11 +49,7 @@ function ProductItem(item) {
         <div>{quantity} {pluralize("item", quantity)} in stock</div>
         <span>${price}</span>
       </div>
-<<<<<<< HEAD
-      <button>Add to cart</button>
-=======
       <button onClick={addToCart}>Add to cart</button>
->>>>>>> feature/global-store
     </div>
   );
 }
